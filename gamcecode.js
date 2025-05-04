7e584c5bd1d170509e36215505f678fd97c80575
joshua cook <jac22855@utexas.edu>
2025-05-04 12:41:26 -0500

    Added a world data and camera object. Now things update with delta time, that is the game works with different refresh rates, and we can more easily change the scale of things using the camera scale and position information. Related, changed all times to be in seconds, and all distances to be number of leafs tall.

--- gamcecode.js
+++ gamcecode.js
@@ -13,33 +13,58 @@ document.addEventListener("keyup", (event) => {
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
+
 var player = {
     position: {
-        x: 10,
-        y: 10
+        x: 0,
+        y: 0
     },
     size: {
-        x: 10,
-        y: 10
+        x: 0.5,
+        y: 1
     },
     velocity: {
         x: 0,
         y: 0
     },
     stats: {
-        jumpHeight: 10
+        jumpHeight: 20,
+        runSpeed: 5
     }
 };
 
-const gravity = -1;
+var worldData = {
+    gravity: -100,
+    time: {
+        currentTime: new Date(),
+        deltaTime: 0,
+    },
+    camera: {
+        position: {x: 0, y: 0},
+        viewSize: 5,
+        canvas: canvas,
+        context: context
+    }
+};
 
-function drawBlock(color, position, size){
-    context.fillStyle = color;
+
+function drawBlock(color, position, size, camera){
+    camera.context.fillStyle = color;
+
+    //Gets the average Screen Size
+    const screenSizeAvg = (camera.canvas.clientHeight + camera.canvas.clientWidth) / 2;
+    const scale = screenSizeAvg / camera.viewSize;
+
     //Translating coordinate system so that y is up and zero is at the bottom.
-    const screenX = position.x;
-        const screenY = canvas.height - position.y;
+    const cameraRelX = position.x - camera.position.x;
+    const cameraRelY = position.y - camera.position.y;
+    const screenX = scale * cameraRelX;
+    const screenY = canvas.height - scale * cameraRelY;
+    const screenWidth = scale * size.x;
+    const screenHeight = scale * size.y;
+
     //position is middle of rectangle.
-    context.fillRect(screenX - size.x/2, screenY - size.y/2, size.x, size.y);
+    context.fillRect(screenX - screenWidth/2, screenY - screenHeight/2, screenWidth, screenHeight);
 }
 
 function isOnGround(character){
@@ -47,33 +72,41 @@ function isOnGround(character){
     return bottom <= 0;
 }
 
+function updateTime(worldTime) {
+    const newTime = new Date();
+    worldTime.deltaTime = (newTime.getTime() - worldTime.currentTime.getTime()) /1000;
+    worldTime.currentTime = newTime;
+}
+
 function update(){
+    updateTime(worldData.time);
     if(keySet.has('a')){
-        player.position.x--;
+        player.position.x-= player.stats.runSpeed * worldData.time.deltaTime;
     }
     if(keySet.has('d')){
-        player.position.x++;
+        player.position.x+= player.stats.runSpeed * worldData.time.deltaTime;
     }
     if(keyDownSet.has('w')){
-        player.velocity.y+=player.stats.jumpHeight;
+        player.velocity.y = player.stats.jumpHeight;
 
-        player.position.y ++;
+        player.position.y += 0.00001;
     }
     if(isOnGround(player)){
         player.position.y = player.size.y/2;
         player.velocity.y = 0;
     }
     else {
-        player.velocity.y+=gravity;
-        player.position.y+=player.velocity.y;
+        player.velocity.y+= worldData.gravity * worldData.time.deltaTime;;
+        player.position.y+= player.velocity.y * worldData.time.deltaTime;
     }
 
     context.fillStyle = "purple";
     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    drawBlock("pink", player.position, player.size);
+    drawBlock("pink", player.position, player.size, worldData.camera);
 
     keyDownSet.clear();
     requestAnimationFrame(update);
 }
+updateTime(worldData.time);
 update();
